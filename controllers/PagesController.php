3c157f4988ef69c1160699f9622559ac0d55fdee
= <=>
2019-02-11 17:06:33 +0100

    requêtes SQL

--- controllers/PagesController.php
+++ controllers/PagesController.php
@@ -7,4 +7,23 @@ class PagesController {
         view('pages.home');
 
     }
+
+    public function divers() {
+
+        echo "<hr>Afficher le nombre de conducteurs.";
+        var_dump(Divers::nombreConducteurs());
+
+        echo "<hr>Afficher le nombre de véhicules.";
+        var_dump(Divers::nombreVehicules());
+
+        echo "<hr>Afficher le nombre d'associations.";
+        var_dump(Divers::nombreAssociations());
+
+        echo "<hr>Afficher les véhicules n'ayant pas de conducteurs.";
+        var_dump(Divers::vehiculesSansConducteurs());
+
+        echo "<hr>Afficher les conducteurs n'ayant pas de véhicules.";
+        var_dump(Divers::conducteursSansVehicules());
+    }
+
 }
--- models/Divers.php
+++ models/Divers.php
@@ -3,6 +3,43 @@
 
 class Divers extends Db {
 
+// Afficher le nombre de conducteurs.
+    public static function nombreConducteurs() {
+        $req = 'SELECT COUNT(*)
+                FROM conducteur';
+        return Db::dbQuery($req);
+    }
 
+    // Afficher le nombre de véhicules.
+    public static function nombreVehicules() {
+        $req = 'SELECT COUNT(*)
+                FROM vehicule';
+        return Db::dbQuery($req);
+    }
 
+    // Afficher le nombre d'associations.
+    public static function nombreAssociations() {
+        $req = 'SELECT COUNT(*)
+                FROM association_vehicule_conducteur';
+        return Db::dbQuery($req);
+    }
+
+    // Afficher les véhicules n'ayant pas de conducteurs.
+    public static function vehiculesSansConducteurs() {
+        $req = 'SELECT *
+                FROM vehicule
+                LEFT JOIN association_vehicule_conducteur ON association_vehicule_conducteur.id_vehicule = vehicule.id
+                WHERE id_conducteur IS NULL';
+        return Db::dbQuery($req);
+    }
+
+    // Afficher les conducteurs n'ayant pas de véhicules.
+    public static function conducteursSansVehicules() {
+        $req = 'SELECT *
+                FROM conducteur
+                LEFT JOIN association_vehicule_conducteur ON association_vehicule_conducteur.id_conducteur = conducteur.id
+                WHERE id_vehicule IS NULL';
+        return Db::dbQuery($req);
+    }
+    
 }
